8bbb8c7ceee74788091dd99ed941f70c3e3c94b3
Simona Chiapperino <simochiay@gmail.com>
2025-09-06 03:07:04 +1000

    Added anime table

--- schema.sql
+++ schema.sql
@@ -12,3 +12,31 @@ INSERT INTO studios (name, country, founded_year) VALUES
 ('Madhouse', 'Japan', 1972);
 
 SELECT * FROM studios;
+
+
+CREATE TABLE IF NOT EXISTS anime (
+    anime_id SERIAL PRIMARY KEY,
+    studio_id INT NOT NULL REFERENCES studios(studio_id),
+    title TEXT NOT NULL,
+    start_year INT
+);
+
+INSERT INTO anime (studio_id, title, start_year) VALUES
+(1, 'Spirited Away', 2001),
+(1, 'My Neighbor Totoro', 1988),
+(2, 'One Piece', 1999),
+(2, 'Dragon Ball Z', 1989),
+(3, 'Naruto', 2002),
+(3, 'Bleach', 2004),
+(4, 'Death Note', 2006),
+(4, 'Hunter x Hunter', 2011);
+
+
+SELECT * FROM anime;
+
+UPDATE anime
+SET start_year = 2001
+WHERE title = 'Spirited Away';
+
+
+
